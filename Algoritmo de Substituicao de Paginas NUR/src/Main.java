fdb2ee90eb63c04cf25ae331a8160c10d255122a
Tonny1993 <96452384+Tonny1993@users.noreply.github.com>
2023-11-27 12:18:02 -0300

    Update Main.java

--- Algoritmo de Substituicao de Paginas NUR/src/Main.java
+++ Algoritmo de Substituicao de Paginas NUR/src/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[]args){
-        System.out.println("\nBem vindo ao Algoritmo de Substituição de Página NUR.\nNosso sistema disponibiliza de 5 molduras para você utilizar, quando essas molduras acabarem, a ultima página modificada será substituída!\nNosso sistema iniciará com 3 páginas criadas.\n");
+        System.out.println("\nBem vindo ao Algoritmo de Substituição de Página NUR.\nNosso sistema disponibiliza 5 molduras para você utilizar, quando essas molduras acabarem, a ultima página modificada será substituída!\nNosso sistema iniciará com 3 páginas criadas.\n");
         AlgoritmoNUR algoritmoNUR=new AlgoritmoNUR();
         algoritmoNUR.menu();
     }
