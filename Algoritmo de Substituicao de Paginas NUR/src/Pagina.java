3f732ad9c4841951b2973cc00a4c6b459279e280
Tonny1993 <96452384+Tonny1993@users.noreply.github.com>
2023-11-27 09:34:02 -0300

    Update Pagina.java

--- Algoritmo de Substituicao de Paginas NUR/src/Pagina.java
+++ Algoritmo de Substituicao de Paginas NUR/src/Pagina.java
@@ -6,14 +6,12 @@ public class Pagina {
     String ultimaExecucao;
     boolean referenciada;
     boolean modificada;
-    boolean substituida;
 
     public Pagina(String nome) {
         this.nome = nome;
         this.ultimaExecucao = obterHoraAtual();
         this.referenciada = true;
         this.modificada = true;
-        this.substituida = false;
     }
 
     public static String obterHoraAtual() {
@@ -31,13 +29,8 @@ public class Pagina {
         this.ultimaExecucao = obterHoraAtual();
     }
 
-    public void atualizarStatusSubstituida(boolean substituida) {
-        this.substituida = substituida;
-        this.ultimaExecucao = obterHoraAtual();
-    }
-
     public String toString() {
-        return String.format("\nNome: %s\nÚltima execução: %s\nReferenciada: %s\nModificada: %s\nSubstituída: %s\n",
-                nome, ultimaExecucao, referenciada, modificada, substituida);
+        return String.format("\nNome: %s\nÚltima execução: %s\nReferenciada: %s\nModificada: %s",
+                nome, ultimaExecucao, referenciada, modificada);
     }
 }
